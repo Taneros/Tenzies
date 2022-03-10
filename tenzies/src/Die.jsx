343fd65a06baf747ef0e12e5bb0afbdccc8dae01
Taneros <fricentaurus@GMAIL.COM>
2022-03-10 16:54:25 +0300

    feat: add dice styles, and render dice

---
+++ tenzies/src/Die.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Die(props) {
+  return (
+    <div className="main--die">
+      <p className="die--num">{props.num}</p>
+    </div>
+  );
+}
--- tenzies/src/Main.jsx
+++ tenzies/src/Main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Die } from "./Die";
 
 export default function Main() {
-  return(
-    <main>
-      
+  function randArr() {
+    return [...Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6))];
+  }
+
+  return (
+    <main className="main">
+      <div className="main--die-wrapper">
+        {randArr().map((val, idx) => (
+          <Die num={val} key={idx} />
+        ))}
+      </div>
     </main>
-  )
+  );
 }
