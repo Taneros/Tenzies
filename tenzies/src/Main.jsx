731ca4592f01178f6835786d4bb2d95e5effd0ab
Taneros <fricentaurus@GMAIL.COM>
2022-03-13 20:51:32 +0300

    feat: add end game logic

--- tenzies/src/Main.jsx
+++ tenzies/src/Main.jsx
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Die } from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function Main() {
   const [dice, setDice] = useState(randArr());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const winArr = [[], []];
+
+    dice.forEach((die) => {
+      winArr[0].push = 1;
+      winArr[1].push(die.value);
+    });
+
+    const allHeld = winArr[0].every((val) => val === true);
+    const sameVal = winArr[1].every((val) => val === winArr[1][0]);
+
+    if (allHeld && sameVal) {
+      setTenzies(true);
+      console.log(`Game Over`);
+    }
+  }, [dice]);
 
   function randArr() {
     return [...Array.from({ length: 10 }, () => randDie())];
   }
 
   function handleRollDice() {
-    setDice((oldDice) => oldDice.map((die) => (die.isHeld ? die : randDie())));
+    if (!tenzies) {
+      setDice((oldDice) =>
+        oldDice.map((die) => (die.isHeld ? die : randDie()))
+      );
+    } else {
+      setDice(randArr());
+      setTenzies((oldTenzies) => !oldTenzies);
+    }
   }
 
   function randDie() {
@@ -48,9 +74,13 @@ export default function Main() {
           />
         ))}
       </div>
-      <button className="main--button" onClick={handleRollDice}>
-        Roll
+      <button
+        className={tenzies ? "main--button __win" : "main--button"}
+        onClick={handleRollDice}
+      >
+        {tenzies ? "New Game" : "Roll"}
       </button>
+      {tenzies && <Confetti />}
     </main>
   );
 }
